d09c71b1e11d5b16646169b34a37dfbc4adfeb13
Matous Slonek <matousslonek@gmail.com>
2024-09-11 20:06:02 +0200

    urls redirect fix

--- api_challenge/countries/urls.py
+++ api_challenge/countries/urls.py
@@ -3,7 +3,7 @@ from django.views.generic import RedirectView
 from .views import CountryDetailView, CountryListView
 
 urlpatterns = [
-    path("", RedirectView.as_view(url="/countries/", permanent=False)),
+    path("", RedirectView.as_view(url="/countries", permanent=False)),
     path('countries/<int:pk>', CountryDetailView.as_view(), name='country-detail'),
     path('countries', CountryListView.as_view(), name='country-list'),
 ]
